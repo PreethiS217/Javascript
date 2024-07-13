e528f7ac507b2510c05d79867e4ac0e1f6c6aa16
PreethiS217 <preethisubramanian21@gmail>
2024-07-13 19:06:57 +0530

    Javascript

---
+++ 02_JS_Variables/Program-05.js
@@ -0,0 +1,16 @@
+var message;
+
+message = "Hello World!";
+console.log(message);
+
+message = 'Congratulations! Your tweet has won a prize...';
+console.log(message);
+
+message = "This is a new message.";
+console.log(message);
+
+message = "Hello " + "World!";
+console.log(message);
+
+message = 'Congratulations! ' + 'Your tweet ' + 'has won a prize...';
+console.log(message);
