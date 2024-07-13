0dade20b894577acb1a3759135715e6ddb982500
PreethiS217 <preethisubramanian21@gmail>
2024-07-13 19:00:54 +0530

    Javascript

---
+++ 02_JS_Variables/Program-03.js
@@ -0,0 +1,7 @@
+var score;
+score = 150; 
+console.log(score);
+
+var score2; 
+score2 = 200; 
+console.log(score2); 
---
+++ 02_JS_Variables/Program-04.js
@@ -0,0 +1,12 @@
+var score;
+score = 100;
+console.log(score);
+score = 150;
+console.log(score);
+score = 200;
+console.log(score); 
+var score2;
+score2 = 300;
+console.log(score2); 
+score2 = 400;
+console.log(score2); 
