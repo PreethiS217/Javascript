061c08424d108606bc3749bc2906326549664358
PreethiS217 <preethisubramanian21@gmail>
2024-07-13 19:17:06 +0530

    Javascript

---
+++ 02_JS_Variables/Program-08.js
@@ -0,0 +1,17 @@
+var score;
+score = 100;
+console.log("Your score was " + score);
+
+console.log("Great splat!!!");
+score = score + 50;
+console.log("New score: " + score);
+
+console.log("Way to go!");
+
+console.log("You splatted a kumquat!");
+
+score = score + 100;
+
+console.log("New score: " + score);
+
+console.log("Congratulations! Keep up the good work!");
