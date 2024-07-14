ea27baf4cf22a010890adb24b79068f0dc7e46f9
PreethiS217 <preethisubramanian21@gmail>
2024-07-14 20:49:08 +0530

    Javascript

---
+++ 08_JS_Array/Program-01.js
@@ -0,0 +1,16 @@
+var scores;
+var names;
+var akaScores;
+
+scores = [ 3, 1, 8, 2 ];
+names = [ "Kandra", "Dax", "Blinky" ];
+
+scores.push(5);
+names.push("Zara");
+
+console.log(scores); 
+console.log(names);  
+
+akaScores = scores;
+
+console.log(akaScores); 
