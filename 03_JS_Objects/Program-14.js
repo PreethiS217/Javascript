3128f3f776f3e4992b0c1bb8e54a95d0e49bd73d
PreethiS217 <preethisubramanian21@gmail>
2024-07-14 12:51:05 +0530

    Javascript

---
+++ 03_JS_Objects/Program-14.js
@@ -0,0 +1,15 @@
+var questionAndAnswer = {
+    question          : "What is the capital of France?",
+    answer1           : "Bordeaux",
+    answer2           : "Lyon",
+    answer3           : "Paris",
+    answer4           : "Brussels",
+    correctAnswer     : "Paris",
+    marksForQuestion  : 2
+};
+
+console.log("Question: " + questionAndAnswer.question);
+console.log("1. " + questionAndAnswer.answer1);
+console.log("2. " + questionAndAnswer.answer2);
+console.log("3. " + questionAndAnswer.answer3);
+console.log("4. " + questionAndAnswer.answer4);
