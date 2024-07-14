38040c9547679575e60568a6101865f578f35348
PreethiS217 <preethisubramanian21@gmail>
2024-07-14 13:47:35 +0530

    Javascript

---
+++ 05_JS_Arguments/Program-03.js
@@ -0,0 +1,10 @@
+var showMessage;
+
+showMessage = function (message) {
+    console.log("The message is: " + message + " - Have a great day!");
+};
+
+showMessage("It's full of stars!"); 
+
+showMessage("The universe is vast!");
+showMessage("Exploration is endless!");
