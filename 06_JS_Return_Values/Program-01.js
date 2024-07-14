c7d8d86a413e609ee8d911937498a7b860eb5491
PreethiS217 <preethisubramanian21@gmail>
2024-07-14 14:23:29 +0530

    Javascript

---
+++ 06_JS_Return_Values/Program-01.js
@@ -0,0 +1,10 @@
+var getMessage = function () {
+    return "I’m going on an adventure!";
+};
+
+var getMyMessage = function () {
+    return "Exploring new horizons!";
+};
+
+var response = getMessage();
+console.log(response);
---
+++ 06_JS_Return_Values/Program-02.js
@@ -0,0 +1,6 @@
+var getHelloTo = function (name1, name2) {
+    return "Hello to " + name1 + " and " + name2;
+};
+
+var fullMessage = getHelloTo("Kandra", "Dax");
+console.log(fullMessage);
