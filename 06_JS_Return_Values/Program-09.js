7a69580f444cef0b8abc26cb93cb1dc481fd5d0c
PreethiS217 <preethisubramanian21@gmail>
2024-07-14 14:48:21 +0530

    Javascript

---
+++ 06_JS_Return_Values/Program-09.js
@@ -0,0 +1,12 @@
+getPlayerInfo = function (playerName = "Unknown Player", playerPlace = "Unknown Place", playerHealth = "Unknown Health") {
+    var playerInfo;
+
+    playerInfo = "\n" + getPlayerName(playerName);
+    playerInfo += "\n" + getBorder();
+    playerInfo += "\n" + getPlayerPlace(playerName, playerPlace);
+    playerInfo += "\n" + getPlayerHealth(playerName, playerHealth);
+    playerInfo += "\n" + getBorder();
+    playerInfo += "\n";
+
+    return playerInfo;
+};
