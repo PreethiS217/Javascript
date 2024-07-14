084542b9f1e6ad34f59812bd654342aaf3b06cab
PreethiS217 <preethisubramanian21@gmail>
2024-07-14 12:57:02 +0530

    Javascript

---
+++ 03_JS_Objects/Program-15.js
@@ -0,0 +1,26 @@
+var player1, player2;
+//Player1
+player1 = {
+    name: "Kandra",
+    health: 50,
+    place: "The Dungeon of Doom",
+    items: ["a rusty key", "The Sword of Destiny", "a piece of cheese"]
+};
+
+console.log(player1.name);
+console.log(player1.name + " is in " + player1.place);
+console.log(player1.name + " has health " + player1.health);
+console.log("Items: " + player1.items.join(", "));
+
+//player2
+player2 = {
+    name: "Grog",
+    health: 75,
+    place: "The Forest of Shadows",
+    items: ["a wooden shield", "a healing potion", "a bow and arrows"]
+};
+
+console.log(player2.name);
+console.log(player2.name + " is in " + player2.place);
+console.log(player2.name + " has health " + player2.health);
+console.log("Items: " + player2.items.join(", "));
