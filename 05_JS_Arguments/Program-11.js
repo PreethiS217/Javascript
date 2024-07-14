9483dbee41974ef1a00a1cf106465d3f87191fe7
PreethiS217 <preethisubramanian21@gmail>
2024-07-14 14:09:12 +0530

    Javascript

---
+++ 05_JS_Arguments/Program-11.js
@@ -0,0 +1,8 @@
+var showPlayerPlace;
+
+showPlayerPlace = function (playerName, playerPlace) {
+    console.log(playerName + " is in " + playerPlace);
+};
+
+showPlayerPlace("Kandra", "The Dungeon of Doom");
+showPlayerPlace("Dax", "The Old Library");
