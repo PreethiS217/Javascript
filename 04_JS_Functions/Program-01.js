2ac75992b0cc7e56b09b29e31a8bf2b0d27c931b
PreethiS217 <preethisubramanian21@gmail>
2024-07-14 13:02:39 +0530

    Javascript

---
+++ 04_JS_Functions/Program-01.js
@@ -0,0 +1,37 @@
+var movie1, movie2;
+
+movie1 = {
+    title: "Inside Out",
+    actors: "Amy Poehler, Bill Hader",
+    directors: "Pete Docter, Ronaldo Del Carmen"
+};
+
+console.log("Movie information for " + movie1.title);
+console.log("------------------------------");
+console.log("Actors: " + movie1.actors);
+console.log("Directors: " + movie1.directors);
+console.log("------------------------------");
+
+movie2 = {
+    title: "The Lion King",
+    actors: "Matthew Broderick, James Earl Jones",
+    directors: "Roger Allers, Rob Minkoff"
+};
+
+console.log("Movie information for " + movie2.title);
+console.log("------------------------------");
+console.log("Actors: " + movie2.actors);
+console.log("Directors: " + movie2.directors);
+console.log("------------------------------");
+
+var blogPost = {
+    title: "A Journey Through the Jungle",
+    author: "Jane Doe",
+    created: "2024-07-14",
+    body: "Today, I explored the heart of the jungle and discovered amazing wildlife and lush landscapes. The experience was unforgettable!"
+};
+
+console.log("Blog Post Title: " + blogPost.title);
+console.log("Author: " + blogPost.author);
+console.log("Created on: " + blogPost.created);
+console.log("Body: " + blogPost.body);
