a6b285397ee140bf74d89db79c8874f32bd6fac6
PreethiS217 <preethisubramanian21@gmail>
2024-07-14 12:39:45 +0530

    Javascript

---
+++ 03_JS_Objects/Program-08.js
@@ -0,0 +1,21 @@
+var book1, book2;
+
+book1 = {
+  title     : "The Hobbit",
+  author    : "J. R. R. Tolkien",
+  published : 1937
+};
+
+console.log(book1.title);
+console.log(book1.author);
+console.log(book1.published);
+
+book2 = {
+  title     : "Northern Lights",
+  author    : "Philip Pullman",
+  published : 1995
+};
+
+console.log(book2.title);
+console.log(book2.author);
+console.log(book2.published);
---
+++ 03_JS_Objects/Program-09.js
@@ -0,0 +1,23 @@
+var book1, book2, book3;
+
+book1 = {
+    title     : "The Hobbit",
+    author    : "J. R. R. Tolkien",
+    published : 1937
+};
+
+book2 = {
+    title     : "Northern Lights",
+    author    : "Philip Pullman",
+    published : 1995
+};
+
+book3 = {
+    title     : "The Catcher in the Rye",
+    author    : "J.D. Salinger",
+    published : 1951
+};
+
+console.log(book1.title + " by " + book1.author + ", published in " + book1.published);
+console.log(book2.title + " by " + book2.author + ", published in " + book2.published);
+console.log(book3.title + " by " + book3.author + ", published in " + book3.published);
