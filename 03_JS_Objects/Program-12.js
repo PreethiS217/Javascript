362cf0899fef238b32ca120b35c543c25852e47f
PreethiS217 <preethisubramanian21@gmail>
2024-07-14 12:45:35 +0530

    Javascript

---
+++ 03_JS_Objects/Program-12.js
@@ -0,0 +1,25 @@
+var post1, post2;
+
+post1 = {
+    id      : 1,
+    title   : "My Crazy Space Adventure",
+    author  : "Philae",
+    created : "2015-06-21",
+    body    : "You will not believe where I just woke up!! Only on a comet..."
+};
+
+console.log("Title: " + post1.title);
+console.log("Author: " + post1.author);
+console.log("Date Created: " + post1.created);
+
+post2 = {
+    id      : 2,
+    title   : "Exploring the Depths of the Ocean",
+    author  : "Deep Diver",
+    created : "2023-07-14",
+    body    : "Today, I discovered a hidden underwater cave filled with colorful corals and mysterious creatures..."
+};
+
+console.log("Title: " + post2.title);
+console.log("Author: " + post2.author);
+console.log("Date Created: " + post2.created);
