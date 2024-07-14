c2051ce74f8e840a2a5bfad620b6bc5f1ccea05a
PreethiS217 <preethisubramanian21@gmail>
2024-07-14 14:04:51 +0530

    Javascript

---
+++ 05_JS_Arguments/Program-10.js
@@ -0,0 +1,23 @@
+var showPlayerHealth;
+
+showPlayerHealth = function (playerName, playerHealth, playerHealthMultiplier) {
+    var adjustedHealth = playerHealth * playerHealthMultiplier;
+    console.log(playerName + " has health " + adjustedHealth);
+};
+
+var player1 = {
+    name: "Kandra",
+    place: "The Dungeon of Doom",
+    health: 50,
+    healthMultiplier: 1.2
+};
+
+var player2 = {
+    name: "Dax",
+    place: "The Old Library",
+    health: 40,
+    healthMultiplier: 1.5
+};
+
+showPlayerHealth(player1.name, player1.health, player1.healthMultiplier);
+showPlayerHealth(player2.name, player2.health, player2.healthMultiplier);
