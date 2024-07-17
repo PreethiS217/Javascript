53b344ffe2cefd490bcb326b426448773bd727d1
PreethiS217 <preethisubramanian21@gmail>
2024-07-17 10:42:27 +0530

    Javascript

---
+++ 09_JS_Constructor/Program-01.js
@@ -0,0 +1,28 @@
+var buildPlanet = function (name, position, type) {
+    var planet = {};
+  
+    planet.name = name;
+    planet.position = position;
+    planet.type = type;
+  
+    return planet;
+};
+
+var planet1 = buildPlanet(
+    "Jupiter",
+    5,
+    "Gas Giant"
+);
+
+console.log(planet1.name); 
+console.log(planet1.position); 
+console.log(planet1.type); 
+
+var planet2 = buildPlanet(
+    "Mars",
+    4,
+    "Terrestrial"
+);
+
+console.log(planet2.name); 
+console.log(planet2.type); 
